92c7208a9050a5072fc37eeffaa6645c50daa652
yzavyalo <yzavyalo@cs.karelia.ru>
2016-06-29 11:18:31 +0300

    working app

--- smartcare/src/main/obj/local/x86/objs/smartcare/SmartSlog/expat/xmltok_impl.o.d
+++
@@ -1,2 +0,0 @@
-/home/cardiacare/AndroidStudioProjects/SmartCareQuestionnaire/smartcare/src/main/obj/local/x86/objs/smartcare/SmartSlog/expat/xmltok_impl.o: \
- /home/cardiacare/AndroidStudioProjects/SmartCareQuestionnaire/smartcare/src/main/jni/SmartSlog/expat/xmltok_impl.c
--- smartcare/src/main/obj/local/x86_64/objs/smartcare/SmartSlog/expat/xmltok_ns.o.d
+++
@@ -1,2 +0,0 @@
-/home/cardiacare/AndroidStudioProjects/SmartCareQuestionnaire/smartcare/src/main/obj/local/x86_64/objs/smartcare/SmartSlog/expat/xmltok_ns.o: \
- /home/cardiacare/AndroidStudioProjects/SmartCareQuestionnaire/smartcare/src/main/jni/SmartSlog/expat/xmltok_ns.c
