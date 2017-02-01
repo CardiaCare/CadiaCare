4fd2151a99858ecb6ca30d768d96e8a2f811ee71
yzavyalo <yzavyalo@cs.karelia.ru>
2017-02-01 15:06:36 +0300

    Bloodpressure diary

--- cardiacare/src/main/java/ru/cardiacare/cardiacare/hisdocuments/BloodPressureActivity.java
+++ cardiacare/src/main/java/ru/cardiacare/cardiacare/hisdocuments/BloodPressureActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,7 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -40,7 +40,7 @@ public class BloodPressureActivity extends AppCompatActivity {
     ListView listView1;
     BPAdapter adapter;
     LinkedList<ResultBloodPressure> bp_data;
-    Button addButton;
+    FloatingActionButton addButton;
     int itemRow;
 
     EditText SYSText;
@@ -107,7 +107,7 @@ public class BloodPressureActivity extends AppCompatActivity {
             }
         });
 
-        addButton = (Button)findViewById(R.id.addBPButton);
+        addButton = (FloatingActionButton)findViewById(R.id.addBPButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
