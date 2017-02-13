74b4de79a7898d430887c8937e6a5607f4cdce2c
yzavyalo <yzavyalo@cs.karelia.ru>
2017-02-13 14:57:00 +0300

    cookiesgit status!

--- cardiacare/src/main/java/ru/cardiacare/cardiacare/survey/FeedbackPOST.java
+++ cardiacare/src/main/java/ru/cardiacare/cardiacare/survey/FeedbackPOST.java
@@ -2,20 +2,29 @@ package ru.cardiacare.cardiacare.survey;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Base64;
 import android.util.Log;
 
-import com.google.gson.Gson;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Credentials;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
 import java.net.HttpURLConnection;
-import java.net.URL;
-
-import java.io.InputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import ru.cardiacare.cardiacare.MainActivity;
 
@@ -37,81 +46,156 @@ public class FeedbackPOST extends AsyncTask<Void, Integer, Integer> {
 
     @Override
     public Integer doInBackground(Void... params) {
-        try {
-            Gson json = new Gson();
-            String jsonFeedback;
-            if (QuestionnaireHelper.questionnaireType.equals("periodic"))
-                jsonFeedback = json.toJson(MainActivity.feedback);
-            else jsonFeedback = json.toJson(MainActivity.alarmFeedback);
-           // jsonFeedback = "{\"user_id\":" + "123456" + ", \"date\":" + "123456"+jsonFeedback+"}";
-            jsonFeedback = "{\"questionnaire_id\":" + "4" + ", \"data\":\"" + jsonFeedback + "\"}";
-            // TODO feedback отправляется всегда, независимо от того, изменились ответы или нет.
-//            String jsonFeedbackOld = readSavedData();
-//            String newfb = jsonFeedback.substring(jsonFeedback.indexOf("personUri"), jsonFeedback.length());
-//            String oldfb = jsonFeedbackOld.substring(jsonFeedbackOld.indexOf("personUri"), jsonFeedbackOld.length());
-//            if (newfb.equals(oldfb)) {
-//                Log.i(MainActivity.TAG, "Feedback не отправлен");
-//                return 0;
-//            }
-            writeData(jsonFeedback);
-
-            //String myURL = "http://api.cardiacare.ru/index.php?r=feedback/create";
-            String myURL = "http://api.cardiacare.ru/patients/"+ MainActivity.storage.getAccountId() +"/feedback";
-            System.out.println("Test!!!!!! account id " + MainActivity.storage.getAccountId());
-            System.out.println("Test!!!!!! json send " + jsonFeedback);
-            /*String parameters;
-            if (QuestionnaireHelper.questionnaireType.equals("periodic"))
-                //parameters = "user_id=" + "123456" + "&date=" + "123456" + "&feedback=" + jsonFeedback;
-                parameters = "questionnaire_id=" + "4";
-            else //parameters = "user_id=" + "654321" + "&date=" + "654321" + "&feedback=" + jsonFeedback;
-                parameters = "questionnaire_id=" + "4";
-            byte[] data = null;*/
-            InputStream is = null;
 
             try {
-                URL url = new URL(myURL);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setDoOutput(true);
-                conn.setDoInput(true);
 
-                //conn.setRequestProperty("Content-Length", "" + Integer.toString(parameters.getBytes().length));
-                conn.setRequestProperty("Content-Type", "application/json");
-                conn.setRequestProperty("Accept", "application/json");
-                conn.setRequestProperty("Authorization", "Basic "+ Base64.encodeToString((MainActivity.storage.getAccountToken()+":").getBytes("UTF-8"), Base64.DEFAULT) );
-                OutputStream os = conn.getOutputStream();
-               /* data = parameters.getBytes("UTF-8");
-                os.write(data);
-                data = null;
-*/
-                conn.connect();
 
-                System.out.println("Test! url " + conn.getResponseCode());
+                CookieManager cookieManager = new CookieManager();
+                cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
 
-                int responseCode = conn.getResponseCode();
+                OkHttpClient client_get = new OkHttpClient();
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                String credential_get = Credentials.basic(MainActivity.storage.getAccountToken(),"");
 
-                if (responseCode == 200) {
-                    is = conn.getInputStream();
+                Request request_get = new Request.Builder()
+                        .url("http://api.cardiacare.ru/patients/14/feedback")
+                        //.post(body)
+                        .header("Authorization", credential_get)
+                        .build();
 
-                    // Такого вот размера буфер
-                    byte[] buffer = new byte[8192];
-                    // Далее, например, вот так читаем ответ
-                    int bytesRead;
-                    while ((bytesRead = is.read(buffer)) != -1) {
-                        baos.write(buffer, 0, bytesRead);
+                client_get.setCookieHandler(cookieManager);
+
+                client_get.newCall(request_get).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        Log.e("response", request.body().toString());
                     }
-//                    data = baos.toByteArray();
-//                    resultString = new String(data, "UTF-8");
-                }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        Log.e("response", response.body().string());
+                    }
+                });
+
+
+
+                MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+                Map<String, String> param;
+                param = new HashMap<String, String>();
+                param.put("questionnaire_id", "4");
+                param.put("data", "4");
+                param.put("version", "4");
+                JSONObject parameter = new JSONObject(param);
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody body = RequestBody.create(JSON, parameter.toString());
+
+                String credential = Credentials.basic(MainActivity.storage.getAccountToken(),"");
+
+                Request request = new Request.Builder()
+                        .url("http://api.cardiacare.ru/patients/14/feedback")
+                        //.post(body)
+                        .header("Authorization", credential)
+                        .addHeader("Content-Length", String.valueOf(parameter.length()))
+                        .addHeader("Cookie", "PHPSESSID=a00eusn4981mj1n93ejd3h4o2pe2rfc0b99dbjodkbfucosb5bp1")
+                        .post(body)
+                        .build();
+
+                client.setFollowRedirects(false|false);
+                client.setCookieHandler(cookieManager);
+
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Request request, IOException e) {
+                        Log.e("response", request.body().toString());
+                    }
+
+                    @Override
+                    public void onResponse(Response response) throws IOException {
+                        Log.e("response", response.body().string());
+                    }
+                });
+
+
+
+//                URL url = new URL("http://api.cardiacare.ru/patients/2/feedback");
+//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                conn.setReadTimeout(10000);
+//                conn.setConnectTimeout(15000);
+//                conn.setRequestMethod("POST");
+//                conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+//                conn.setUseCaches (false);
+//                conn.setDoOutput(true);
+//                conn.setDoInput(true);
+//
+//                conn.setRequestProperty("Authorization", String.format("Basic %s", Base64.encodeToString((MainActivity.storage.getAccountToken()+":").getBytes("UTF-8"), Base64.DEFAULT) ));
+//
+//                conn.setRequestProperty("Content-Type", "application/json");
+
+
+//                Map<String, List<String>> hdrs = conn.getHeaderFields();
+//                Set<String> hdrKeys = hdrs.keySet();
+//                for (String k : hdrKeys)
+//                    System.out.println("Key: " + k + "  Value: " + hdrs.get(k));
+
+                // Send post request
+//                conn.setDoOutput(true);
+//                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+//                wr.write("{" +
+//                        "  \"questionnaire_id\": 4,\n" +
+//                        "  \"version\": \"0.1.1\",\n" +
+//                        "  \"data\": \"new questionnaire\"\n" +
+//                        "\n" +
+//                        "}");
+//                wr.flush();
+//                wr.close();
+//
+//                int responseCode = conn.getResponseCode();
+//
+//                System.out.println("Response Code : " + responseCode);
+//
+//                BufferedReader in = new BufferedReader(
+//                        new InputStreamReader(conn.getInputStream()));
+//                String inputLine;
+//                StringBuffer response = new StringBuffer();
+//
+//                while ((inputLine = in.readLine()) != null) {
+//                    response.append(inputLine);
+//                }
+//                in.close();
+//
+//
+//                System.out.println(response.toString());
+//
+//                DataOutputStream wr = new DataOutputStream(conn.getOutputStream ());
+//                wr.writeBytes("{" +
+//                        "  \"questionnaire_id\": 4,\n" +
+//                        "  \"version\": \"0.1.1\",\n" +
+//                        "  \"data\": \"new questionnaire\"\n" +
+//                        "\n" +
+//                        "}");
+//                wr.flush();
+//                wr.close();
+//
+//                Log.i("RESP","response " + conn.getResponseCode());
+//
+//                int responseCode = conn.getResponseCode();
+//
+//                InputStream stream = conn.getInputStream();
+//                int sz = stream.available();
+//                byte[] b = new byte[sz];
+//                stream.read(b);
+//                stream.close();
+//                String data = new String(b);
+//
+//                Log.i("RESP","response " + data);
+//                if (responseCode == 200) {
+//
+//                }
             } catch (Exception e) {
-//                resultString = "Exception:" + e.getMessage();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Log.i(MainActivity.TAG, "Feedback отправлен");
+
         return 0;
     }
 
