2e56a42db2d82877c090a779dd610a15f380c183
Bassilij <fok-wassya@yandex.ru>
2017-02-23 19:20:24 +0300

    Create Account Post

--- cardiacare/src/main/java/ru/cardiacare/cardiacare/user/CreateAccountPost.java
+++ cardiacare/src/main/java/ru/cardiacare/cardiacare/user/CreateAccountPost.java
@@ -1,13 +1,12 @@
-package ru.cardiacare.cardiacare.idt_ecg;
+package ru.cardiacare.cardiacare.user;
+
 
 import android.os.AsyncTask;
-import android.util.Base64;
 import android.util.Log;
 
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Credentials;
 import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.MultipartBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
@@ -15,21 +14,13 @@ import com.squareup.okhttp.Response;
 
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import ru.cardiacare.cardiacare.MainActivity;
 
-/* Отправка данных ЭКГ на сервер */
+public class CreateAccountPost  extends AsyncTask<JSONObject, String, String> {
 
-public class ECGPost extends AsyncTask<JSONObject, String, String> {
-
-    public static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("text/x-markdown; charset=utf-8");
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     @Override
     protected void onPreExecute() {
@@ -40,33 +31,18 @@ public class ECGPost extends AsyncTask<JSONObject, String, String> {
     protected String doInBackground(JSONObject... params) {
 
         try {
-            String dirPath = "/";
-            File fileq = MainActivity.mContext.getFilesDir();//new File(dirPath);
-            File[] files = fileq.listFiles();
-            for(int i = 0; i < files.length; i++)
-                System.out.println("Test! files " + files[i].toString());
-
             OkHttpClient client = new OkHttpClient();
 
-            //File file = new File("feedback.json"); // MainActivity.storage.getECGFile() //questionnaire.json
+            String json =  params[0].toString();
+            System.out.println("Test! json " + json);
+           // String json = "{ \"email\":" + CreateAccountActivity.etLogin.getText().toString();
 
-//            File file = new File(MainActivity.mContext.getFilesDir(), MainActivity.storage.getECGFile());
-            File file = new File(MainActivity.mContext.getFilesDir(), "ecgfile1");
-            //File file = new File(MainActivity.mContext.getFilesDir(), "lbp.txt");
-
-            System.out.println("Test! file " + file.toString());
-
-            RequestBody body = RequestBody.create(MEDIA_TYPE_MARKDOWN, file);
+            RequestBody body = RequestBody.create(JSON, json);
 
             System.out.println("Test! body " + body.toString());
 
-            String credential = Credentials.basic(MainActivity.storage.getAccountToken(), "");
-
-            System.out.println("Test! token " + credential);
-
             Request request = new Request.Builder()
-                    .url("http://api.cardiacare.ru/biosignals")
-                    .addHeader("Authorization", credential)
+                    .url("http://api.cardiacare.ru/users")
                     .post(body)
                     .build();
 
